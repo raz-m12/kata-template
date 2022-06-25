e123484cc35c8e2518a937468fd81ce63f0b2739
razfv12@gmail.com <razfv12@gmail.com>
2022-06-25 21:17:31 +0200

    Defaults to 3 spaces indent length

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@ cd extern
 vcpkgDir=./vcpkg
 
 if [ -d "$vcpkgDir" ]; then
-    echo "Vcpkg directory already exists"
+   echo "Vcpkg directory already exists"
 else
-    # Install the package manager vcpkg and use it to satisfy
-    # the dependencies reported in the manifest file (vcpkg.json)
-    git clone https://github.com/microsoft/vcpkg
-    cd vcpkg && ./bootstrap-vcpkg.sh -disableMetrics
-    ./bootstrap-vcpkg.sh
+   # Install the package manager vcpkg and use it to satisfy
+   # the dependencies reported in the manifest file (vcpkg.json)
+   git clone https://github.com/microsoft/vcpkg
+   cd vcpkg && ./bootstrap-vcpkg.sh -disableMetrics
+   ./bootstrap-vcpkg.sh
 
-    # Installs the dependencies system wide
-    ./vcpkg integrate install
+   # Installs the dependencies system wide
+   ./vcpkg integrate install
 fi
