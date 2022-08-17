f7b6d43314a6787d94e99fc4576ec5aefc920b8d
razfv12@gmail.com <razfv12@gmail.com>
2022-08-17 19:51:08 +0200

    Revert "Adds code coverage tools such as llvm-cov and gcov"
    This reverts commit 6b59de44c4fce597d2cc8e7f0ef6e23959a348b2.

--- install.sh
+++ install.sh
@@ -12,7 +12,6 @@ else
     sudo apt install cppcheck
     sudo apt install clang-tidy
     sudo apt install clang-format
-    sudo apt install llvm
 
     # Install the package manager vcpkg and use it to satisfy
     # the dependencies reported in the manifest file (vcpkg.json)
