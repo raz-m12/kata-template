fbb843637695f24e44393bf9e8154ac3347086d1
razfv12@gmail.com <razfv12@gmail.com>
2022-08-26 20:46:15 +0200

    Adds user defined global config file

--- src/Argument.cpp
+++ src/Argument.cpp
@@ -9,6 +9,6 @@ auto BoolArgument::getValueProected() -> any { return _value; }
 IntArgument::IntArgument(int value) : _value{value} {}
 auto IntArgument::getValueProected() -> any { return _value; }
 
-StringArgument::StringArgument(string value) : _value{std::move(value)} {}
+StringArgument::StringArgument(string value) : _value{move(value)} {}
 auto StringArgument::getValueProected() -> any { return _value; }
 }  // namespace args::libs
